0e5f8e3968daff4ca8e51fcbfb673b4308dacb5c
Jonathan Warner <jaxbot@gmail.com>
2019-03-25 20:13:44 -0700

    Working

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -8,7 +8,6 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
@@ -20,37 +19,42 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-
     int white_pixel = 255;
+    int ball_x = -1, ball_y = -1, white_pixel_count = 0;
+    float white_pixel_cutoff = 0.70;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
-    int ball_x = -1, ball_y = -1;
+    // Loop through each pixel looking for a bright white (r,b,g=255) one.
+    // Keep track of the number of pixels that fit this criteria.
     for (int i = 0; i < img.height * img.step; i++) {
         if (img.data[i] - white_pixel == 0) {
             ball_y = i / img.step;
             ball_x = (i % img.step) / (img.step / img.width);
-            break;
+            white_pixel_count += 1;
         }
     }
 
-    if (ball_x != -1 && ball_y != -1) {
-        ROS_INFO_STREAM("Found ball at " + std::to_string(ball_x) + "," + std::to_string(ball_y) + "," + std::to_string(img.step / img.width));
-        int third = img.width / 3;
+    // ENHNACEMENT: Calculate the % of pixels that are white. This is used to stop the
+    // robot when the white ball is touching the nose.
+    float white_pixel_percent = (float)white_pixel_count / (img.height * img.step);
+    ROS_INFO_STREAM("White pixel percent: " + std::to_string(white_pixel_percent));
+
+    if (ball_x != -1 && ball_y != -1 && white_pixel_percent < 0.70) {
+        float third = img.width / 3;
+	float ang_z;
         if (ball_x < third) {
-            // go left
-            ROS_INFO_STREAM("Going left");
-            drive_robot(0, 0.5);
+            // Pixel is in the left third, so go left at an appropriate angle.
+            ang_z = 0.5 * (third / (ball_x + 1));
+            ROS_INFO_STREAM("Going left: " + std::to_string(ang_z));
+            drive_robot(0.5, ang_z);
         } else if (ball_x < third * 2) {
-            // go straight
+            // Pixel is in the middle third, so go straight.
             ROS_INFO_STREAM("Going straight");
             drive_robot(0.5, 0);
         } else {
-            // go right
-            ROS_INFO_STREAM("Going right");
-            drive_robot(0, -0.5);
+            // Pixel is in the right third, so go right at an appropriate angle.
+            ang_z = -0.5 * ball_x / img.width;
+            ROS_INFO_STREAM("Going right: " + std::to_string(ang_z));
+            drive_robot(0.5, ang_z);
         }
     } else {
         drive_robot(0, 0);
