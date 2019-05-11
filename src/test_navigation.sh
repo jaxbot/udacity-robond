675ff68408f13fa7706064acdea87bb97499e9be
Jonathan Warner <jaxbot@gmail.com>
2019-05-11 06:32:43 -0700

    Change initial pose

--- src/test_navigation.sh
+++ src/test_navigation.sh
@@ -1,6 +1,7 @@
 source ../devel/setup.bash
 export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/my_robot/worlds/jonathan.world"
 export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/my_robot/maps/map.yaml"
+export ROBOT_INITIAL_POSE="-x 2 -y 0 -z 0"
 #xterm  -e  " gazebo $TURTLEBOT_GAZEBO_WORLD_FILE" &
 xterm -e "roslaunch turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch" &
 sleep 5
