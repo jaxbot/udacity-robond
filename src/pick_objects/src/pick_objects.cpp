f5c249b69093b9d491e2e3a0d99554723ad2f0c1
Jonathan Warner <jaxbot@gmail.com>
2019-05-14 18:03:08 -0700

    Clean up pick_objects

--- src/pick_objects/src/pick_objects.cpp
+++ src/pick_objects/src/pick_objects.cpp
@@ -8,6 +8,7 @@ float PICKUP_X = 8;
 float PICKUP_Y = 2.68;
 float DROPOFF_X = 1.4;
 float DROPOFF_Y = -7;
+
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 ros::ServiceClient addMarkerClient;
@@ -15,24 +16,24 @@ ros::ServiceClient clearMarkerClient;
 
 bool move_to_x_y(float x, float y) {
   MoveBaseClient ac("move_base", true);
-  // Wait 5 sec for move_base action server to come up
+  // Wait for the MoveBaseClient to init.
   while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
+    ROS_INFO("Still waiting for the MoveBaseClient to ready.");
   }
+
   move_base_msgs::MoveBaseGoal goal;
 
-  // set up the frame parameters
+  // Pin to the map frame_id
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
-  // Define a position and orientation for the robot to reach
+  // Set the goal to the provided x, y values.
   goal.target_pose.pose.position.x = x;
   goal.target_pose.pose.position.y = y;
   goal.target_pose.pose.orientation.w = 1.0;
 
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal");
-
+  ROS_INFO("Sending goal for robot to reach.");
+  // Send the goal request and wait for it to complete (or fail).
   ac.sendGoal(goal);
   ac.waitForResult();
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
@@ -44,35 +45,6 @@ bool move_to_x_y(float x, float y) {
   }
 }
 
-bool jitter() {
-  MoveBaseClient ac("move_base", true);
-  // Wait 5 sec for move_base action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-  move_base_msgs::MoveBaseGoal goal;
-
-  // set up the frame parameters
-  goal.target_pose.header.frame_id = "base_link";
-  goal.target_pose.header.stamp = ros::Time::now();
-
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 1;
-  goal.target_pose.pose.orientation.w = 1.0;
-
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal");
-
-  ac.sendGoal(goal);
-  ac.waitForResult();
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Moved to goal successfully.");
-    return true;
-  } else {
-    ROS_INFO("Failed to move to goal.");
-    return false;
-  }
-}
 void show_marker(float x, float y) {
   add_markers::AddMarkers srv;
   srv.request.x = x;
@@ -86,20 +58,24 @@ void clear_marker() {
 }
 
 int main(int argc, char** argv){
-  // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "simple_navigation_goals");
+  ros::init(argc, argv, "pick_objects");
   ros::NodeHandle n;
 
-  //tell the action client that we want to spin a thread by default
-
+  // Create clients for addMarker/clearMarker to display the goal nodes in rviz.
   addMarkerClient = n.serviceClient<add_markers::AddMarkers>("/add_marker/add_marker");
   clearMarkerClient = n.serviceClient<add_markers::ClearMarker>("/add_marker/clear_marker");
 
+  // Show the pickup zone marker.
   show_marker(PICKUP_X, PICKUP_Y);
+  // Move to the pickup zone.
   move_to_x_y(PICKUP_X, PICKUP_Y);
+  // Clear the marker now that we've reached the pickup zone.
   clear_marker();
+  // Wait 5 seconds to simulate the pickup.
   ros::Duration(5.0).sleep();
+  // Move to the dropoff zone.
   move_to_x_y(DROPOFF_X, DROPOFF_Y);
+  // Show the marker now that we've reached the dropoff zone.
   show_marker(DROPOFF_X, DROPOFF_Y);
 
   return 0;
